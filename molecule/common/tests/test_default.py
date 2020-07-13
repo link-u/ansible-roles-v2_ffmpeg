e186d1726ff956c0fb647e2d965c92f54a00225e
y-hashida <yoshimasa.hashida@link-u.co.jp>
2020-07-14 01:07:11 +0900

    update: ffmpeg と ffprobe のテストランを追加.

--- molecule/common/tests/test_default.py
+++ molecule/common/tests/test_default.py
@@ -7,3 +7,7 @@ testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
 
 def test_installed_cavif_and_davif(host):
     assert host.package("ffmpeg-tools").is_installed
+
+def test_run_help_cavif_and_davif(host):
+    assert host.run("ffmpeg --help").succeeded
+    assert host.run("ffprobe --help").succeeded

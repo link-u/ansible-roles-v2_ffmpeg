ec4739d4031c6818d692e13a78e409a2ed414de7
y-hashida <yoshimasa.hashida@link-u.co.jp>
2020-07-18 04:13:53 +0900

    update: test の関数名を変更

--- molecule/common/tests/test_default.py
+++ molecule/common/tests/test_default.py
@@ -4,9 +4,9 @@ import testinfra.utils.ansible_runner
 testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
     os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
 
-def test_installed_cavif_and_davif(host):
+def test_installed_package(host):
     assert host.package("ffmpeg-tools").is_installed
 
-def test_run_help_cavif_and_davif(host):
+def test_run_help_command(host):
     assert host.run("ffmpeg --help").succeeded
     assert host.run("ffprobe --help").succeeded
